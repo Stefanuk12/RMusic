1b4f107a8faf865520a55b0b703660bfc6f81381
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2021-01-30 02:42:30 +0000

    Added MusicTable Array and methods

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
 // Dependencies
 import got from "got/dist/source";
-import { IRMusicAudio } from "./interfaces/audio";
+import { IRMusicAudio } from "./interfaces/IRMusicAudio";
 
 export class RMusic {
     // Vars
-    static removedAssets: Array<string> = [
+    MusicTable: Array<IRMusicAudio> = [];
+    removedAssets: Array<string> = [
         "https://images.rbxcdn.com/9281912c23312bc0d08ab750afa588cc.png",
         "https://t6.rbxcdn.com/70608418c648be7ac4e323e3294bb059",
         "https://t5.rbxcdn.com/d28c1b5eed271a7aa76f16689e74ca04"
     ];
 
 
-    // Check if an audio has been removed or not - returns an error if it has, else returns nothing
+    // Filter that checks through the audios
     private checkFilter(webSource: string){
-        for (const removedAsset of RMusic.removedAssets){
+        for (const removedAsset of this.removedAssets){
             if (webSource.includes(removedAsset)){
                 return false;
             }
@@ -21,29 +22,107 @@ export class RMusic {
 
         return true;
     }
-    async check(audio: IRMusicAudio, xcsrftoken: string = "", filter: Function = this.checkFilter){
+
+    // Check if audios are still valid
+    async check(audio: IRMusicAudio, filter: Function = this.checkFilter){
         try {
             // Get webpage source
             const webSource = await got.get(`https://roblox.com/library/${audio.id}`, {
-                throwHttpErrors: false,
-                headers: {
-                    "x-csrf-token": xcsrftoken
-                }
+                throwHttpErrors: false
             });
             
             // 404
             if (webSource.statusCode == 404){
-                throw(new Error("Audio does not exist"))
+                throw(new Error("Audio does not exist."))
             }
 
             // Checking if it has been removed then returning
             const success = filter(webSource.body);
             if (success == false){
-                throw(new Error("Audio failed"))
+                throw(new Error("Audio failed."))
             }
         } catch(error) {
-            // Return
-            return error;
+            // Throw Error
+            throw (error);
         }
+    }
+
+    // Add Audio to table
+    async add(audio: IRMusicAudio, checkAudio: boolean = false, filter: Function = this.checkFilter, musicTable: Array<IRMusicAudio> = this.MusicTable){
+        // Check if the audio is alive
+        if (checkAudio){
+            try {
+                await this.check(audio);
+                musicTable.push(audio);
+                return;
+            } catch(err) {
+                throw(err);
+            }
+        }
+
+        //
+        musicTable.push(audio);
+    }
+
+    // Find an audio
+    find(audio: IRMusicAudio, musicTable: Array<IRMusicAudio> = this.MusicTable){
+        for (const _audio of musicTable){
+            if (_audio.id == audio.id){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Remove audio from table
+    remove(audio: IRMusicAudio, musicTable: Array<IRMusicAudio> = this.MusicTable){
+        for (var i = 0; i < musicTable.length; i++){
+            const _audio = musicTable[i];
+
+            if (audio == _audio){
+                musicTable.splice(i, 1);
+            }
+        }
+    }
+
+    // Remove duplicates from table
+    removeDuplicates(musicTable: Array<IRMusicAudio> = this.MusicTable){
+        var cleanArray: Array<IRMusicAudio> = [];
+
+        for (const audio of musicTable){
+            if (!this.find(audio, cleanArray)){
+                cleanArray.push(audio);
+            }
+        }
+
+        return cleanArray;
+    }
+
+    // Alphabetically sort the table
+    alphabetSort(musicTable: Array<IRMusicAudio> = this.MusicTable){
+        return musicTable.sort((a, b) => {
+            if (a.name && b.name){
+                if (a.name < b.name) return -1;
+                if (a.name > b.name) return 1;
+            }
+
+            return 0;
+        })
+    }
+
+    // Check entire music table
+    async checkAll(musicTable: Array<IRMusicAudio> = this.MusicTable){
+        var cleanArray: Array<IRMusicAudio> = [];
+
+        for (const audio of musicTable){
+            try {
+                await this.check(audio);
+                cleanArray.push(audio);
+                return;
+            } catch(err) {}
+        }
+
+        return cleanArray;
     }
 }
